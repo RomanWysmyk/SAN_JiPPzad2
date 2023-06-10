cd221e49c64672ffeab1a7171253f90bf4e6242d
Roman <wysmykroman@gmail.com>
2023-06-10 08:21:11 +0200

    Dodanie komentarzy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 public class Main {
+
+    //Longest Collatz Sequence - która początkowa liczba poniżej miliona stworzy najdłuższy łańcuch
     public static void main(String[] args) {
         long limit = 1000000;
         long longestChainNumber = 0;
         long longestChainLength = 0;
 
+
+        // pętla leci przez zmienną limit (1milion) Wewątrz pętla while sprawdza czy zmienna number nie jest  równa 1
+        // i stosuje wzór łańcucha przy każdej iteracji zwiękrzając chainLenght. Dodatkowo If sprawdza i podmienia
+        // zmienną longestChainNumber na tę najdłuższą.
         for (long i = 1; i < limit; i++) {
             long number = i;
             long chainLength = 1;
